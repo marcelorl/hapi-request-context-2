133d5eeb4729c2c644e10ca16a2783e295505da6
Christian Hubinger <christian@trigo.at>
2015-06-24 00:32:09 +0200

    Remove executable flag

--- specs/injectWrapper.js
+++ specs/injectWrapper.js
@@ -3,13 +3,13 @@
 var bb = require('bluebird');
 
 module.exports = function injectWrapper(server) {
-    var inject = {
-        inject: function (args, cb) {
-            server.inject(args, function (res) {
-                cb(null, res);
-            });
-        }
-    };
+	var inject = {
+		inject: function (args, cb) {
+			server.inject(args, function (res) {
+				cb(null, res);
+			});
+		}
+	};
 	bb.promisifyAll(inject);
 
 	return inject;
